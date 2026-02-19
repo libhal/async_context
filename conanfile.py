50ff7762cc003fedaa41fbf52c8dc669af72979a
Khalil Estell <kammce0@gmail.com>
2026-02-19 08:54:57 -0800

    :pushpin: libhal-cmake-util/5.0.3

--- conanfile.py
+++ conanfile.py
@@ -99,7 +99,7 @@ class async_context_conan(ConanFile):
     def build_requirements(self):
         self.tool_requires("cmake/[^4.0.0]")
         self.tool_requires("ninja/[^1.3.0]")
-        self.requires("libhal-cmake-util/[^5.0.2]")
+        self.requires("libhal-cmake-util/[^5.0.3]")
         if str(self.settings.os) != "baremetal":
             self.test_requires("boost-ext-ut/2.3.1")
             self.test_requires("benchmark/1.9.4")
